ea44e2254bba3b4a9413170b14e9fa2b59ce458e
Guna Sekhar <gunasekhar234@gmail.com>
2026-08-25 14:52:29 +0530

    smoke test removed for one test

--- tests/ecommerce/add-product2.spec.js
+++ tests/ecommerce/add-product2.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '../../fixtures/testFixtures';
 
-test('Add one more backpack to cart @smoke', async ({ productPage }) => {
+test('Add one more backpack to cart ', async ({ productPage }) => {
 
     await productPage.navigate();
 
